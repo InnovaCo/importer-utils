20ad73edba3df693e2794c202382d697aec5347b
Sergey Chikuyonok <serge.che@gmail.com>
2014-08-25 19:48:11 +0400

    Использование абстракции `Resource` для работы с файлами

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var file = require('./lib/file');
 var common = require('./lib/common');
+var Resource = require('./lib/resource');
 
 common.extend(module.exports, common);
 module.exports.file = file;
+module.exports.Resource = Resource;
